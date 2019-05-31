d67acf516a419512ef9553228210fd71a801943e
shirokuro <46kuro212@gmail.com>
2019-05-31 19:59:59 +0900

    feat: rewrite code

--- src/main.rs
+++ src/main.rs
@@ -1,200 +1,170 @@
 #[macro_use]
-extern crate log;
-#[macro_use]
 extern crate serde_derive;
 
-use std::env;
 use std::fs::File;
-use std::io;
-use std::io::BufReader;
-use std::io::Read;
-use std::io::Write;
-use std::path::Path;
-use std::path::PathBuf;
+use std::io::{BufReader, Read, Write};
+use std::path::{Path, PathBuf};
 
-use chrono::Local;
-use env_logger;
 use tar::Builder;
-
-use backup::{Condition, Filter, Target};
-
-mod backup;
+use walkdir::WalkDir;
+use chrono::Local;
 
 fn main() {
-    for arg in std::env::args() {
-        if let "--version" = arg.as_str() {
-            println!(env!("CARGO_PKG_VERSION"));
-            return;
-        }
-    }
+    println!("Loading settings...");
+    let settings = load_settings();
+    println!("Successfully loaded!\nBacking up...");
+    execute(&settings);
+    println!("Successfully Backed up!");
+}
 
-    initialize_logger();
+fn execute(settings: &Settings) {
+    let archive_file_name = Local::now().format(&settings.archive_file_path).to_string();
+    let mut archive_file = File::create(archive_file_name)
+        .expect("Failed to create archive file");
+    let mut archiver = Builder::new(&mut archive_file);
 
-    let mut archiver;
-    let targets: Vec<Target>;
-    let filters: Vec<Filter>;
-    let commands_after_backup: Vec<Vec<String>>;
-    let archive_path;
-    //initialization
-    {
-        let settings = Settings::load();
-        archive_path = Local::now().format(settings.archive_path.as_str()).to_string();
-        archiver = prepare_start(archive_path.as_str());
-
-        targets = settings.targets.into_iter()
-            .map(|setting| setting.into_target())
+    for target in &settings.targets {
+        println!("Current target: {}({})", &target.name, &target.path);
+        let mut filterable_dir: Option<PathBuf> = Option::None;
+        let filters: Vec<&Filter> = settings.filters.iter()
+            .filter(|filter| filter.applied_to.eq(&target.name))
             .collect();
-        filters = settings.filters.unwrap_or_default()
-            .into_iter()
-            .map(|setting| setting.into_filter())
-            .collect();
-        commands_after_backup = settings.commands_after_backup.unwrap_or_default();
-    }
 
-    backup::start(targets.as_slice(),
-                  filters.as_slice(),
-                  &mut archiver);
-    archiver.into_inner().expect("Failed to finish the archiver")
-        .flush().expect("Flush failed");
-    backup::execute_commands(&commands_after_backup.as_slice(),
-                             &dunce::realpath(archive_path).unwrap().to_string_lossy());
-}
+        'outer: for entry in WalkDir::new(&target.path) {
+            let entry = entry.expect("Failed to unwrap");
+            let entry_path = entry.path();
 
-fn initialize_logger() {
-    let log_level_env_key = "RUST_LOG";
-    if env::var(log_level_env_key).is_err() {
-        env::set_var(log_level_env_key, "INFO");
-    }
-
-    env_logger::Builder::from_default_env().format(|formatter, record| {
-        use log::Level;
-        use colored::Colorize;
-
-        let level = record.level();
-        let level_string = format!("{}", level);
-        let level_str = level_string.as_str();
-        let level_str = match level {
-            Level::Debug | Level::Trace => level_str.bright_black(),
-            Level::Warn => level_str.yellow(),
-            Level::Error => level_str.bright_red(),
-            _ => colored::ColoredString::from(level_str)
-        };
-        //1: timestamp 2: level 3: body
-        writeln!(formatter, "[{} {}] {}",
-                 Local::now().format("%Y-%m-%d %H:%M:%S"),
-                 level_str,
-                 record.args())
-    }).init();
-
-    debug!("Logger initialized.");
-}
-
-fn prepare_start(archive_path: &str) -> Builder<File> {
-    let file_path = Path::new(&archive_path);
-    if file_path.exists() {
-        println!("File: \"{}\" already exists!", archive_path);
-
-        //Lock and unlock stdout
-        {
-            let stdout = std::io::stdout();
-            let mut stdout = stdout.lock();
-
-            stdout.write_all(b"(O)verwirte, (E)xit: ").unwrap();
-            stdout.flush().unwrap();
-        }
-
-        let mut input = String::new();
-        io::stdin().read_line(&mut input).expect("Failed to read line!");
-        if input.trim().to_ascii_lowercase() != "o" {
-            println!("Exiting...");
-            std::process::exit(0);
-        }
-    }
-    let file = File::create(archive_path).expect("Failed to create archive file.");
-
-    Builder::new(file)
-}
-
-#[derive(Deserialize, Debug)]
-struct FilterSetting {
-    name: String,
-    scopes: Vec<String>,
-    targets: Vec<String>,
-    conditions: Vec<String>,
-}
-
-impl FilterSetting {
-    fn into_filter(self) -> Filter {
-        let targets: Vec<PathBuf> = self.targets.iter()
-            .map(|path| Path::new(path).to_path_buf())
-            .collect();
-        let conditions: Vec<Condition> = self.conditions.iter().map(|condition_str| {
-            let not = condition_str.starts_with('!');
-            let condition_str = if not {
-                condition_str.replacen("!", "", 1)
-            } else {
-                condition_str.to_string()
-            };
-            let path = Path::new(&condition_str).to_path_buf();
-
-            Condition {
-                not,
-                path,
+            if entry_path.is_dir() && is_filterable_dir(&filters, entry_path) {
+                filterable_dir = Option::Some(entry_path.to_path_buf());
             }
-        }).collect();
 
-        Filter {
-            name: self.name,
-            scopes: self.scopes,
-            targets,
-            conditions,
+            if filterable_dir.is_some() &&
+                entry_path.starts_with(&filterable_dir.as_ref().unwrap()) {
+                continue 'outer
+            }
+
+            execute_dir_entry(entry_path, target, &mut archiver);
         }
     }
+
+    let need_flush = archiver.into_inner().expect("Failed to finish the archiver");
+    need_flush.flush().expect("Failed to flush the archive file");
 }
 
-#[derive(Deserialize, Debug)]
-struct TargetSetting {
-    name: String,
-    paths: Vec<String>,
-}
+fn is_filterable_dir(filters: &[&Filter], path: &Path) -> bool {
+    for filter in filters {
+        let parent = path.parent()
+            .expect("Cannot get parent path");
 
-impl TargetSetting {
-    fn into_target(self) -> Target {
-        let paths: Vec<PathBuf> = self.paths.iter()
-            .map(|path| dunce::canonicalize(Path::new(path)).unwrap())
-            .collect();
-
-        Target {
-            name: self.name,
-            paths,
+        if append_paths(parent, &filter.if_exists).exists() {
+            let path_buf = &path.to_path_buf();
+            for exclude_relative_path in &filter.exclude {
+                if append_paths(parent, exclude_relative_path).eq(path_buf) {
+                    println!("  Filtered({}): {}", &filter.name, path.display());
+                    return true
+                }
+            }
         }
     }
+
+    false
+}
+
+fn append_paths(parent: &Path, child: &str) -> PathBuf {
+    let mut path_buf = parent.to_path_buf();
+    path_buf.push(child);
+
+    path_buf
+}
+
+fn execute_dir_entry(
+    entry_path: &Path,
+    target: &Target,
+    archiver: &mut Builder<&mut File>
+) {
+    if !entry_path.is_file() {
+        return
+    }
+
+    let archive_to = {
+        let mut result = PathBuf::new();
+        result.push(&target.name);
+        result.push(trim_duplicated(entry_path, Path::new(&target.path)));
+
+        result
+    };
+
+    let mut opened_file = File::open(entry_path)
+        .expect("Failed to open file");
+    archiver.append_file(archive_to, &mut opened_file)
+        .expect("Failed to archive file");
+}
+
+fn trim_duplicated(target: &Path, filter: &Path) -> PathBuf {
+    let mut result = PathBuf::new();
+    let mut target_iter = target.iter();
+    let mut filter_iter = filter.iter();
+
+    loop {
+        let target_next = target_iter.next();
+        if target_next.is_none() {
+            break
+        }
+        let filter_next = filter_iter.next();
+        let target_next = target_next.unwrap();
+        if filter_next.is_some() && target_next.eq(filter_next.unwrap()) {
+            continue
+        }
+
+        result.push(target_next);
+    }
+
+    result
+}
+
+fn load_settings() -> Settings {
+    let settings_path = Path::new("./settings.toml");
+    if !settings_path.exists() {
+        eprintln!("./settings.toml does not exists!");
+        File::create(settings_path).expect("Failed to create settings file");
+        std::process::exit(0)
+    }
+
+    let opened_file = File::open(settings_path).expect("Failed to open ./settings.toml");
+
+    let settings: Settings = {
+        // Loads settings
+
+        let mut reader = BufReader::new(opened_file);
+        let mut buf = String::new();
+        reader.read_to_string(&mut buf).expect("Failed to read lines from settings.toml");
+
+        toml::from_str(&buf.as_str()).expect("Failed to deserialize settings")
+    };
+
+    settings
 }
 
 #[derive(Deserialize, Debug)]
 struct Settings {
-    archive_path: String,
-    targets: Vec<TargetSetting>,
-    filters: Option<Vec<FilterSetting>>,
-    commands_after_backup: Option<Vec<Vec<String>>>,
+    #[serde(rename = "archive-file-path")]
+    archive_file_path: String,
+    targets: Vec<Target>,
+    filters: Vec<Filter>,
 }
 
-impl Settings {
-    pub fn load() -> Settings {
-        let settings_path = Path::new("./settings.toml");
-        if !settings_path.exists() {
-            println!("Settings file not exists! creating it and exiting...");
-            File::create(settings_path).expect("Failed to create settings.toml.");
-            std::process::exit(1);
-        }
-        let settings_file = File::open("settings.toml").expect("Failed to open settings.toml");
-        let mut reader = BufReader::new(settings_file);
-
-        let mut settings_buffer = String::new();
-        reader.read_to_string(&mut settings_buffer).expect("Failed to read lines from settings.toml");
-        let settings: Settings = toml::from_str(&settings_buffer.as_str()).expect("Failed to deserialize settings.");
-
-        settings
-    }
+#[derive(Deserialize, Debug)]
+struct Target {
+    name: String,
+    path: String,
 }
 
+#[derive(Deserialize, Debug)]
+struct Filter {
+    name: String,
+    #[serde(rename = "applied-to")]
+    applied_to: String,
+    exclude: Vec<String>,
+    #[serde(rename = "if-exists")]
+    if_exists: String,
+}
